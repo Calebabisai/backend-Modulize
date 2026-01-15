eef8720853c5c3ba43838be3e175d7e636dba26a
Calebabisai <gelndcaleb@gmail.com>
2026-01-15 00:26:34 -0600

    Configuracion correcta de prisma agregada. Solucion de error en Prisma 7. Se ha movido la URL de schema.prisma a prisma.config.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { PrismaService } from './src/prisma/prisma/prisma.service';
+import { PrismaService } from './prisma/services/prisma.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { CommonModule } from './common/common.module';
+import { MaterialsModule } from './materials/materials.module';
+import { ProjectsModule } from './projects/projects.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UsersModule, PrismaModule, CommonModule, MaterialsModule, ProjectsModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
