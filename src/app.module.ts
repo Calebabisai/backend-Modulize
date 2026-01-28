4cad9ec00c9d9997306698fad13b2d1a2dcc997e
Calebabisai <gelndcaleb@gmail.com>
2026-01-28 01:18:50 -0600

    Se ha eliminado el import duplicado de PrismaService ene app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,15 +3,21 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
-import { PrismaService } from './src/prisma/prisma/prisma.service';
-import { PrismaService } from './prisma/services/prisma.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { CommonModule } from './common/common.module';
 import { MaterialsModule } from './materials/materials.module';
 import { ProjectsModule } from './projects/projects.module';
+import { PrismaService } from './prisma/services/prisma.service';
 
 @Module({
-  imports: [AuthModule, UsersModule, PrismaModule, CommonModule, MaterialsModule, ProjectsModule],
+  imports: [
+    AuthModule,
+    UsersModule,
+    PrismaModule,
+    CommonModule,
+    MaterialsModule,
+    ProjectsModule,
+  ],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
